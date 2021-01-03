eb9bb55c56499f85f12a9ea56f00a1e61885877b
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-01-03 20:55:13 +0200

    Header with a nice picture added, which could later be turned into a button

--- src/Head/Head.js
+++ src/Head/Head.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import "./Head.css";
+// import pfp from './WaltPFP.png' //later have users upload one
 
 const header = (props) => {
     return (
         <header className='Head'>
             <ul id='header_list'>
-                <li id='site_name'><h1>The Royal Duck</h1> </li>
-                <li id='profile_button'><h4>Profile</h4></li>
+                <li id='site_name'><h1>Golf Royale</h1> </li>
+                <li id='profile_image'><img src="https://images.fd.nl/archive/77923_nieuwe-bronnen2web.jpg?fit=crop&crop=faces&auto=format%2Ccompress&q=45&w=360&h=360&fm=jpg&cs=tinysrgb" width="100px" height="100px"></img></li>
             </ul>
         </header>
 
