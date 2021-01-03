a31062ad36049d4c1b3fb15e2b25e6ea81dca524
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-01-03 16:37:41 +0200

    creating the header. Basic version.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import User from "./User/User";
+import Header from "./Head/Head";
 
 class App extends Component {
-  //Only availible in components that extends component
+  //STATE CONTAINS THE DATA FROM SERVER/DATABASE
   state = {
-    users : [
-      {name: 'WALT', winnings: 1},
-      {name: 'LYNN', winnings: 99},
-    ]
-  }
-
-  switchNameHandler = () => {
-    //console.log('was clicked')
-    // extends component gives setState 
-    this.setState({
-      users : [
-        {name: 'WALT', winnings: 99},
-        {name: 'LYNN', winnings: 1},
-      ]
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Welcome to golf Royal</h1>
-        <button onClick={this.switchNameHandler}>Press me</button>
-        <User name={this.state.users[0].name} age={this.state.users[0].winnings}>Slight of hand</User>
-        <User name={this.state.users[1].name} age={this.state.users[1].winnings}>Cutting someone in half</User>
+        <Header/>
       </div>
     );
   }
---
+++ src/Head/Head.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./Head.css";
+
+const header = (props) => {
+    return (
+        <header className='Head'>
+            <ul id='header_list'>
+                <li id='site_name'><h1>The Royal Duck</h1> </li>
+                <li id='profile_button'><h4>Profile</h4></li>
+            </ul>
+        </header>
+
+    )
+};
+
+export default header;
