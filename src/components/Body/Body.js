05f422d6912912032746aae291b1c2ef71fb0e6d
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-01-04 13:26:21 +0200

    Added the Body component to host everything on the main screen. This makes the App.js file a bit neater.

---
+++ src/components/Body/Body.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './Body.css'
+import Players from './Chart/Players'
+
+const body = (props) => {
+
+    return (
+        <div>
+            <ul>
+                <li id="nav-side"><div className='Temp'></div></li>
+                <li id="chart-side">
+                    <ul>
+                        <li><div className='Temp2'></div></li>
+                        <li>
+                            <div>
+                                {props.persons.map((person, index) => {
+                                    return (< Players
+                                        Click={() => { this.addScoreHandler(person.id) }}
+                                        name={person.name}
+                                        score={person.score}
+                                        key={person.id} />
+                                    )
+                                })}
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default body;
