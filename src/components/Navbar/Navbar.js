535246672a3dfe74ebecab8204ebcb1986cc8f0e
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-01-09 20:59:43 +0200

    NavBar text decoration. Basically I made it black.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,11 +9,11 @@ const navBar = (props) => {
     return (
         <div className='Navbar'>
             <ul className="Tiles">
-                <li><Link to="/"><h3>Home</h3></Link></li>
-                <li><Link to="/user-profile"><h3>Profile</h3></Link></li>
-                <li><Link to="/user-stats"><h3>Stats</h3></Link></li>
-                <li><Link><h3>icon</h3></Link></li>
-                <li><Link><h3>icon</h3></Link></li>
+                <li><Link to="/"><h4>Home</h4></Link></li>
+                <li><Link to="/user-profile"><h4>Profile</h4></Link></li>
+                <li><Link to="/user-stats"><h4>Stats</h4></Link></li>
+                <li><Link><h4>icon</h4></Link></li>
+                <li><Link><h4>icon</h4></Link></li>
             </ul>
         </div>
     );
