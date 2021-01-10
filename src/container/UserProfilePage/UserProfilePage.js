8db1e47976c9fd24f99399238e4eec68eab7f118
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-01-10 17:31:01 +0200

    added colour and picture input field, and save button (onClick to be added)

--- src/container/UserProfilePage/UserProfilePage.js
+++ src/container/UserProfilePage/UserProfilePage.js
@@ -5,7 +5,10 @@ class ProfilePage extends Component {
     render(){
 
         // TO DO: get past info of user
-        // TO DO: add button to save
+        // TO DO: save this info to database when button clicked
+        // TO DO: use picture selected here in header
+
+        // minor issue: box around button when clicked
 
         return(
             <div>
@@ -16,11 +19,55 @@ class ProfilePage extends Component {
                             <tr class="row1">
                                 <td class="prompt">Nickname:</td>
                                 <td>
-                                    <input type="text" name="name" id="name"></input>
+                                    <input type="text" name="name" id="name" ></input>
+                                </td>
+                            </tr>
+                            <tr class="row2">
+                                <td class="prompt">Colour:</td>
+                                <select name="color" id="color" class="entryfield">
+                                    <option value="Yellow">
+                                    Yellow
+                                    </option>
+
+                                    <option value="Blue">
+                                    Blue
+                                    </option>
+
+                                    <option value="Gray">
+                                    Black
+                                    </option>
+
+                                    <option value="Red">
+                                    Red
+                                    </option>
+
+                                    <option value="Green">
+                                    Green
+                                    </option>
+
+                                    <option value="Purple">
+                                    Purple
+                                    </option>
+
+                                    <option value="Pink">
+                                    Pink
+                                    </option>
+
+                                </select>
+
+                            </tr>
+                            <tr class="row1">
+                                <td class="prompt">Picture URL:</td>
+                                <td>
+                                    <input type="text" name="picture" id="picture" ></input>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
+                    <h5>Don't forget to save your changes!</h5>
+                    <button id="ProfileSave">
+                        Save
+                    </button>
                 </div>     
             </div>
         );
