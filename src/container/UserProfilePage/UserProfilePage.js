e5cbb7008a52a2af98f426308c3239b66d46dd7d
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-01-10 15:27:23 +0200

    The starting blocks of the profile page: an input field for your nickname

--- src/container/UserProfilePage/UserProfilePage.js
+++ src/container/UserProfilePage/UserProfilePage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import './UserProfilePage.css'
 
 class ProfilePage extends Component {
     render(){
+
+        // TO DO: get past info of user
+        // TO DO: add button to save
+
         return(
             <div>
                 <h1>PROFILE</h1>
+                <div class="leftbox">
+                    <table border="0" cellSpacing="0" cellPadding="0">
+                        <tbody>
+                            <tr class="row1">
+                                <td class="prompt">Nickname:</td>
+                                <td>
+                                    <input type="text" name="name" id="name"></input>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>     
             </div>
         );
     }
