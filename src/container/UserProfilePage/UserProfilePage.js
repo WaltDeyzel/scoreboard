48f50dbffcfb8940cfc7570cdf9bed55146aa01a
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-01-15 18:13:23 +0200

    added remaining basic components to card: best game and score ratio. Still need to be screen-size adjusted.

--- src/container/UserProfilePage/UserProfilePage.js
+++ src/container/UserProfilePage/UserProfilePage.js
@@ -8,6 +8,10 @@ class ProfilePage extends Component {
         // TO DO: save this info to database when button clicked
         // TO DO: use picture selected here in header
         // TO DO: limit amount of chars of nickname
+        // TO DO: add best game from database
+        // TO DO: make this card generic so it can be used on other pages
+        // TO DO: fix scaling
+        // TO DO: make button do stuff
 
         // minor issue: box around button when clicked
 
@@ -71,8 +75,13 @@ class ProfilePage extends Component {
                     </button>
                 </div>     
                 <div class="rightbox"> {/* this box also acts as a border for the card */}
-                <div id="userName">user name</div>
+                    <div id="userName">Walt</div>
                     <img id="profilePic" src="https://images.fd.nl/archive/77923_nieuwe-bronnen2web.jpg?fit=crop&crop=faces&auto=format%2Ccompress&q=45&w=360&h=360&fm=jpg&cs=tinysrgb"></img>
+                    <div id="strength">Best game: Chess</div>
+                    
+                    <div id="winRatio">9/10</div>
+                    <div id="winRatioPrompt">wins/total games</div>
+                    
                 </div>
 
 
