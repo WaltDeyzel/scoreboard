b7181e56067a0d70e1a8ad82336000c7904cc6bc
Lynn Weyn <WYNLYN004@myuct.ac.za>
2021-01-19 19:49:00 +0200

    Added Firebase configuration

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCffv4e0A5psMT4jACNJHMKRb3W_ncJRjg",
+    authDomain: "scoreboard-143.firebaseapp.com",
+    projectId: "scoreboard-143",
+    storageBucket: "scoreboard-143.appspot.com",
+    messagingSenderId: "1065749242327",
+    appId: "1:1065749242327:web:1563367d58713ffe106b92"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
